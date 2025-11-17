55827dc8fdde611970e3746791663794409f1815
adamek9431 <a.wieckowski.1994@gmail.com>
2025-11-17 22:25:04 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white px-4">
+      <h1 className="text-center max-w-4xl">
+        Chcesz kupić tę domenę? Zadzwoń{" "}
+        <a href="tel:+48694435251" className="hover:underline">
+          +48 694 435 251
+        </a>
+      </h1>
+    </div>
+  );
+}
